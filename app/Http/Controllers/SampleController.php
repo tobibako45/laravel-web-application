e1028fe01e80ae0c74c07e14de39ce3a13c64a0e
tobibako45 <tobibako45@gmail.com>
2019-07-02 10:14:35 +0900

    Change CalculationService

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use App\Facades\Hoge;
 
 use App\Facades\Calculation;
-// use App\Services\CalculationService;
+
+use App\Services\CalculationService;
 
 
 class SampleController extends Controller
@@ -18,6 +19,7 @@ class SampleController extends Controller
     //     $this->Calculation = $calculation;
     // }
 
+    // public function index(CalculationService $calculation)
     public function index()
     {
 
@@ -31,22 +33,28 @@ class SampleController extends Controller
         $result = [
 
             // 直接サービスコンテナから
-            // 'add' => $calculation->add(5, 1),
-            // 'sub' => $calculation->sub(6, 1),
-            // 'mul' => $calculation->mul(7, 2),
-            // 'div' => $calculation->div(8, 2),
+            // 'add' => $calculation->add(1, 1),
+            // 'sub' => $calculation->sub(2, 1),
+            // 'mul' => $calculation->mul(3, 2),
+            // 'div' => $calculation->div(4, 2),
 
             // コンストラクタインジェクション
-            // 'add' => $this->Calculation->add(1, 1),
-            // 'sub' => $this->Calculation->sub(2, 1),
-            // 'mul' => $this->Calculation->mul(3, 2),
-            // 'div' => $this->Calculation->div(4, 2),
+            // 'add' => $this->Calculation->add(5, 1),
+            // 'sub' => $this->Calculation->sub(6, 1),
+            // 'mul' => $this->Calculation->mul(7, 2),
+            // 'div' => $this->Calculation->div(8, 2),
+
+            // メソッドインジェクション
+            // 'add' => $calculation->add(9, 1),
+            // 'sub' => $calculation->sub(10, 1),
+            // 'mul' => $calculation->mul(11, 2),
+            // 'div' => $calculation->div(12, 2),
 
             // ファサードで
-            'add' => Calculation::add(9, 1),
-            'sub' => Calculation::sub(10, 1),
-            'mul' => Calculation::mul(11, 2),
-            'div' => Calculation::div(12, 2),
+            'add' => Calculation::add(13, 1),
+            'sub' => Calculation::sub(14, 1),
+            'mul' => Calculation::mul(15, 2),
+            'div' => Calculation::div(16, 2),
         ];
 
 
